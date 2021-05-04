8603d310f91a3705d804d75f1596f6dc73bd0bfd
Tainá Inaiê <taina.inaie@gmail.com>
2021-05-03 21:17:01 -0300

    Summary
    Atualização das páginas html

---
+++ quinto-ano/digital-influencer/digital-influencer-05.php
@@ -0,0 +1,84 @@
+<?php  
+include_once("../../header.php");
+?>
+
+<header>
+	<style type="text/css">
+		.hidden
+		{
+			display: none;
+		}
+		input
+		{
+			display: block;
+		}
+	</style>
+</header>
+	
+	<div class="container">
+		<div class="row">
+	    	<div class="col-sm">
+	    		<div class="text-center">
+			    	<h2>Digital Influencer</h2>
+			    	<hr></hr>
+	    		</div>
+	    	</div>
+	    </div>
+
+	    <div class="row">
+	    	<div class="col-sm">
+	    		<div class="text-center">
+	    			<input class="btn btn-primary" type="button" value="Webcam" onclick="mostrar('webcam')" />
+	    			<div class="hidden" id="webcam">
+	      				<iframe src="webcam.html" width="640px" height="480px" align="center"></iframe>
+	      			</div>
+	    		</div>	
+	    	</div>
+	    </div>	
+
+	    <br>
+
+		<div class="row">
+	    	<div class="col-sm">
+	    		<div class="text-center">
+	    			<img src="../../imagem/digital-influencer.png" class="img-fluid" alt="Imagem responsiva" width="300" height="400">
+	    		</div>
+	    	</div>
+	    </div>
+
+	    <div class="row">	
+	    	<div class="col-sm" >
+    			<div class="text-center">
+					<p><h5>She works by transforming what she knows into interesting,<br>
+					attractive and valuable content for her followers.</h5></p>
+		    	</div>
+	    	</div>
+	    </div>
+
+		<div class="row">
+			<div class="col-6 text-center">
+				<a class="btn btn-primary" href="../digital-influencer/digital-influencer-04.php" role="button">Prev</a>	
+		    </div>
+
+	      	<div class="col-6 text-center">
+	      		<a class="btn btn-primary" href="../digital-influencer/digital-influencer-05.php" role="button">Next</a>
+	      	</div>	
+		</div>
+	</div>		
+
+	<script type="text/javascript">
+		function mostrar(obj)
+		{
+			if (document.getElementById(obj).style.display == 'block')
+			{
+				document.getElementById(obj).style.display = 'none';
+			}
+			else
+			{
+				document.getElementById(obj).style.display = 'block';
+			}
+		}
+	</script>
+<?php  
+include_once("../../footer.php");
+?>
